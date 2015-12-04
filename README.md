d56e61ee59fceeda586f0efadb6520a14db64754
wengqi <qiweng@wisedu.com>
2015-12-04 22:59:19 +0800

    init

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
-# webapp
-基于jquery等常规技术的前端工程架构范例
+# 概要
+基于jquery等常规技术的前端工程架构范例（单页应用）
 # 目录结构
 ```
 webapp
     |-- app  //开发目录
     |    |-- assets  //存放非代码性静态资源，如字体、图片、视频、音频等等
+    |    |-- components  //自定义组件, 组件相关html、js、css、img全部放在此目录下的某组件文件夹，便于统一管理
     |    |-- lib  //第三方类库
     |    |-- mock  //模拟后台数据，格式为标准json文件
     |    |-- scripts  //js代码
@@ -13,7 +14,6 @@ webapp
     |    |      |      |-- constant.js  //定义系统常量
     |    |      |      |-- services.js  //服务类
     |    |      |      |-- utils.js  //工具类
-    |    |      |-- components  //自定义组件
     |    |      |-- page1  //模块1目录（根据业务模块重命名）
     |    |      |-- page2  //模块2目录（根据业务模块重命名）
     |    |      |-- ...    //其他业务模块目录（根据业务模块重命名）
@@ -21,13 +21,15 @@ webapp
     |    |      |-- routes.js  //前端路由
     |    |-- styles  //样式
     |    |-- views  //模块页面/模板
+    |    |      |-- common  //公共页面
+    |    |      |-- page1  //页面1
     |    |-- index.html  //入口首页
     |-- dist  //编译输出目录
     |-- test  //单元测试
     |-- node_modules  //nodejs依赖模块
     |-- gulpfiles.js  //gulp配置文件
     |-- package.json //nodejs包管理文件
-    |-- README.md
+    |-- README.md  //工程说明文档
     |-- webpack.config.js //webpack配置文件
 
 ```
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,30 @@
 var webpack = require('webpack');
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
-var path = require('path');
-var glob_entries = require('webpack-glob-entries');
 
 module.exports = {
-  entry: glob_entries('./examples/scripts/*.js'), //demo演示
-  //entry: glob_entries('./src/**/*.js'),//发布时使用，可以将组建独立打包
+  entry: './app/scripts/index.js',
 
   resolve: {
     //自动扩展文件后缀名，意味着我们require模块可以省略不写后缀名
-    extensions: ['', '.js', '.jsx', '.json', '.scss', '.css']
+    extensions: ['', '.js', '.css']
   },
 
   output: {
-    //path: path.join(process.cwd(), 'dist'),// 可以注释掉，使用gulp中定义的dest
     filename: '[name].js'
   },
 
   module: {
     loaders: [
       {
-        test: [/\.jsx$/, /\.js$/],
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        query:{
-          "compact": false,
-          "presets" : ['es2015','react']
-        }
-      }, 
-      {
         test: /\.json$/,
         loader: 'json-loader'
-      }, 
-      {
-        test: /\.less$/,
-        loader: ExtractTextPlugin.extract(
-            'style-loader',"css-loader!less-loader?sourceMap"
-        )
-      }, 
-      {
-        test: /\.scss$/,
-        loader: ExtractTextPlugin.extract(
-            'style-loader',"css-loader!sass-loader?sourceMap"
-        )
-      }, 
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract(
-            'style-loader', 'css-loader'
-        )
       }
     ]
   },
 
   //发布打包组建时需要注释CommonsChunkPlugin插件，使得每个插件能完整独立可用
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin('common.js'),
-    new ExtractTextPlugin('[name].css')
+    new webpack.optimize.CommonsChunkPlugin('common.js')
   ],
 
   devtool: 'source-map'
